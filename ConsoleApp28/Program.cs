b0b92548760dac3723f219b942d6830657a5ec0f
Danila <Danila@DESKTOP-B319KEU>
2023-03-01 12:46:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Swap<T>(ref T first, ref T second)
+        {
+            T buf = first;
+            first = second;
+            second = buf;
+        }
+        static void Main(string[] args)
+        {
+            int first = 15, second = 20;
+            Swap(ref first, ref second);
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+        }
+    }
+}
